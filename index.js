4d9677fcdbbcfe54aa9dddc751e8c96b82f36ec9
特定非営利活動法人　コネクト <Hirofumi.Matsumoto@connect-npo.org>
2025-05-15 21:15:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -84,3 +84,6 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`🚀 Server is running on port ${port}`);
 });
+app.get('/', (req, res) => {
+  res.send('こころちゃんは元気です🌸');
+});
