5bf200ecf5fbec7aca3af928784eaf94470061a3
特定非営利活動法人　コネクト <Hirofumi.Matsumoto@connect-npo.org>
2025-05-15 19:30:26 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+  "name": "kokoro-chat",
+  "version": "1.0.0",
+  "main": "index.js",
+  "type": "commonjs",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "axios": "^1.6.8",
+    "@line/bot-sdk": "^7.5.1",
+    "openai": "^4.20.0",
+    "dotenv": "^16.4.5"
+  }
+}
